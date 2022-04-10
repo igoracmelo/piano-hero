bcbbda558ea9d70465c75dc8bec94e804ee0ccd5
Igor Melo <igoracmelo@protonmail.com>
2022-04-09 23:08:40 -0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,8 @@ type Key = {
   displayedKeymap: string,
   keymap: string,
   note: string,
-  sharp: boolean
+  sharp: boolean,
+  semitones: number
 }
 
 export default defineComponent({
@@ -50,30 +51,61 @@ export default defineComponent({
     return {
       audioContext: null as AudioContext | null,
       gainControl: null as GainNode | null,
-      currentOctave: 5,
+      currentOctave: 2,
+      keyNodes: {
+        A1: null,
+        AS1: null,
+        B1: null,
+        C2: null,
+        CS2: null,
+        D2: null,
+        DS2: null,
+        E2: null,
+        F2: null,
+        FS2: null,
+        G2: null,
+        GS2: null,
+        A2: null,
+        AS2: null,
+        B2: null,
+        C3: null,
+        CS3: null,
+        D3: null,
+        DS3: null,
+        E3: null,
+        F3: null
+      } as Record<string, GainNode|null>,
       keys: [
-        { active: false, displayedKeymap: 'A', keymap: 'KeyA', note: 'A1', sharp: false },
-        { active: false, displayedKeymap: 'W', keymap: 'KeyW', note: 'AS1', sharp: true },
-        { active: false, displayedKeymap: 'S', keymap: 'KeyS', note: 'B1', sharp: false },
-        { active: false, displayedKeymap: 'D', keymap: 'KeyD', note: 'C2', sharp: false },
-        { active: false, displayedKeymap: 'R', keymap: 'KeyR', note: 'CS2', sharp: true },
-        { active: false, displayedKeymap: 'F', keymap: 'KeyF', note: 'D2', sharp: false },
-        { active: false, displayedKeymap: 'T', keymap: 'KeyT', note: 'DS2', sharp: true },
-        { active: false, displayedKeymap: 'G', keymap: 'KeyG', note: 'E2', sharp: false },
-        { active: false, displayedKeymap: 'H', keymap: 'KeyH', note: 'F2', sharp: false },
-        { active: false, displayedKeymap: 'U', keymap: 'KeyU', note: 'FS2', sharp: true },
-        { active: false, displayedKeymap: 'J', keymap: 'KeyJ', note: 'G2', sharp: false },
-        { active: false, displayedKeymap: 'I', keymap: 'KeyI', note: 'GS2', sharp: true },
-        { active: false, displayedKeymap: 'K', keymap: 'KeyK', note: 'A2', sharp: false },
-        { active: false, displayedKeymap: 'O', keymap: 'KeyO', note: 'AS2', sharp: true },
-        { active: false, displayedKeymap: 'L', keymap: 'KeyL', note: 'B2', sharp: false },
-        { active: false, displayedKeymap: 'Ç', keymap: 'Semicolon', note: 'C3', sharp: false },
-        { active: false, displayedKeymap: '´', keymap: 'BracketLeft', note: 'CS3', sharp: true },
-        { active: false, displayedKeymap: '~', keymap: 'Quote', note: 'D3', sharp: false },
-        { active: false, displayedKeymap: '[', keymap: 'BracketRight', note: 'DS3', sharp: true },
-        { active: false, displayedKeymap: ']', keymap: 'Backslash', note: 'E3', sharp: false },
-        { active: false, displayedKeymap: 'Enter', keymap: 'Enter', note: 'F3', sharp: false }
-      ]
+        { active: false, displayedKeymap: 'A', keymap: 'KeyA', note: 'A1', sharp: false, semitones: 0 },
+        { active: false, displayedKeymap: 'W', keymap: 'KeyW', note: 'AS1', sharp: true, semitones: 1 },
+        { active: false, displayedKeymap: 'S', keymap: 'KeyS', note: 'B1', sharp: false, semitones: 2 },
+        { active: false, displayedKeymap: 'D', keymap: 'KeyD', note: 'C2', sharp: false, semitones: 3 },
+        { active: false, displayedKeymap: 'R', keymap: 'KeyR', note: 'CS2', sharp: true, semitones: 4 },
+        { active: false, displayedKeymap: 'F', keymap: 'KeyF', note: 'D2', sharp: false, semitones: 5 },
+        { active: false, displayedKeymap: 'T', keymap: 'KeyT', note: 'DS2', sharp: true, semitones: 6 },
+        { active: false, displayedKeymap: 'G', keymap: 'KeyG', note: 'E2', sharp: false, semitones: 7 },
+        { active: false, displayedKeymap: 'H', keymap: 'KeyH', note: 'F2', sharp: false, semitones: 8 },
+        { active: false, displayedKeymap: 'U', keymap: 'KeyU', note: 'FS2', sharp: true, semitones: 9 },
+        { active: false, displayedKeymap: 'J', keymap: 'KeyJ', note: 'G2', sharp: false, semitones: 10 },
+        { active: false, displayedKeymap: 'I', keymap: 'KeyI', note: 'GS2', sharp: true, semitones: 11 },
+        { active: false, displayedKeymap: 'K', keymap: 'KeyK', note: 'A2', sharp: false, semitones: 12 },
+        { active: false, displayedKeymap: 'O', keymap: 'KeyO', note: 'AS2', sharp: true, semitones: 13 },
+        { active: false, displayedKeymap: 'L', keymap: 'KeyL', note: 'B2', sharp: false, semitones: 14 },
+        { active: false, displayedKeymap: 'Ç', keymap: 'Semicolon', note: 'C3', sharp: false, semitones: 15 },
+        { active: false, displayedKeymap: '´', keymap: 'BracketLeft', note: 'CS3', sharp: true, semitones: 16 },
+        { active: false, displayedKeymap: '~', keymap: 'Quote', note: 'D3', sharp: false, semitones: 17 },
+        { active: false, displayedKeymap: '[', keymap: 'BracketRight', note: 'DS3', sharp: true, semitones: 18 },
+        { active: false, displayedKeymap: ']', keymap: 'Backslash', note: 'E3', sharp: false, semitones: 19 },
+        { active: false, displayedKeymap: 'Enter', keymap: 'Enter', note: 'F3', sharp: false, semitones: 20 },
+        { active: false, displayedKeymap: '7', keymap: 'Numpad7', note: 'FS3', sharp: true, semitones: 21 },
+        { active: false, displayedKeymap: '4', keymap: 'Numpad4', note: 'G3', sharp: false, semitones: 22 },
+        { active: false, displayedKeymap: '8', keymap: 'Numpad8', note: 'GS3', sharp: true, semitones: 23 },
+        { active: false, displayedKeymap: '5', keymap: 'Numpad5', note: 'A3', sharp: false, semitones: 24 }
+        //
+        // App.vue?47b3:104
+        // App.vue?47b3:104
+        // App.vue?47b3:104
+      ] as Key[]
     }
   },
 
@@ -90,15 +122,19 @@ export default defineComponent({
 
     this.gainControl = this.audioContext.createGain()
 
-    this.gainControl.gain.setValueAtTime(0.05, 0)
+    this.gainControl.gain.setValueAtTime(0.1, 0)
     this.gainControl.connect(this.audioContext.destination)
 
     window.addEventListener('keydown', (e) => {
       if (e.repeat) return
 
+      if (e.code.startsWith('Digit')) {
+        this.currentOctave = parseInt(e.code.slice(-1))
+      }
+
       const key = this.keys.find(key => key.keymap === e.code)
 
-      if (key) {
+      if (key && !this.isPlaying(key)) {
         key.active = true
         this.play(key)
         // const audioSource = this.audioContext.createBufferSource()
@@ -111,22 +147,67 @@ export default defineComponent({
     window.addEventListener('keyup', (e) => {
       const key = this.keys.find(key => key.keymap === e.code)
 
-      if (key) {
+      console.log(e.code)
+
+      if (key && this.isPlaying(key)) {
         key.active = false
+        this.stop(key)
       }
     })
   },
 
   methods: {
+
+    // createFadeOut (duration?: number): GainNode {
+    //   if (!this.audioContext) throw new Error()
+
+    //   const fadeOut = this.audioContext.createGain()
+    //   // fadeOut.gain.setValueAtTime(1, 0)
+
+    //   return fadeOut
+    // },
+
+    isPlaying (key: Key): boolean {
+      return !!this.keyNodes[key.note]
+    },
+
     play (key: Key) {
       if (!this.audioContext || !this.gainControl) return
 
-      const octave = parseInt(key.note.slice(-1))
+      const fadeOut = this.audioContext.createGain()
+      fadeOut.connect(this.gainControl)
+
+      // const startFadeAt = this.audioContext.currentTime + 10
+      // setTimeout(() => this.stop(key), startFadeAt)
+      // fadeOut.gain.setValueAtTime(1, startFadeAt)
+      // fadeOut.gain.exponentialRampToValueAtTime(0.001, startFadeAt + 0.5)
+
       const oscillator = this.audioContext.createOscillator()
-      oscillator.frequency.value = 440.0 * (2 ** octave) // this.currentOctave
-      oscillator.connect(this.gainControl)
+      oscillator.connect(fadeOut)
+
+      this.keyNodes[key.note] = fadeOut
+
+      const semitoneFactor = 2.0 ** (1 / 12)
+      const firstNote = 440.0 * (2 ** (this.currentOctave - 2))
+      const frequency = firstNote * semitoneFactor ** key.semitones
+
+      oscillator.frequency.value = frequency
+      // oscillator.connect(this.gainControl)
       oscillator.start()
-      oscillator.stop(this.audioContext.currentTime + 0.3)
+    },
+
+    stop (key: Key) {
+      const fadeOut = this.keyNodes[key.note]
+      if (!this.audioContext || !this.gainControl || !fadeOut) return
+
+      const stopAt = this.audioContext.currentTime + 1
+      // fadeOut.gain.setValueAtTime(1, 0)
+      fadeOut.gain.exponentialRampToValueAtTime(0.001, stopAt)
+
+      this.keyNodes[key.note] = null
+      // const fadeOut = this.createFadeOut(stopAt)
+      // oscillator.connect(fadeOut)
+      // oscillator.stop(stopAt)
     },
 
     playSnare () {
@@ -150,11 +231,15 @@ $key-width: 50px
   -moz-osx-font-smoothing: grayscale
   text-align: center
   color: #2c3e50
-  margin-top: 60px
+  // margin-top: 60px
   padding: 10px
-  display: flex
-  flex-direction: column
-  align-items: center
+  display: grid
+  place-items: center
+  width: 100%
+  height: 100vh
+  // flex-direction: column
+  // align-items: center
+  background: #5af
 
 .keyboard
   display: flex
@@ -165,7 +250,7 @@ $key-width: 50px
   height: 300px
   padding: 20px
   border-radius: 10px
-  background: #5af
+  // background: #5af
 
 .keys
   display: flex
